3a56c607e57e7c438833ca0e9b84ad112ae385be
csavelief <csavelief@mncc.fr>
2025-09-02 09:28:36 +0200

    Mark 'invitation' emails as deprecated

--- app/Mail/Invitation.php
+++ app/Mail/Invitation.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
+/** @deprecated */
 class Invitation extends Mailable
 {
     use Queueable, SerializesModels;
