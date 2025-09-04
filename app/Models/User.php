f90c089fa8625488d2642c6c3f8d89d18ae361d7
csavelief <csavelief@mncc.fr>
2025-09-04 11:40:13 +0200

    Disable automatic Superset account creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -133,11 +133,11 @@ class User extends WaveUser
 
             $user->syncRoles(Role::ADMINISTRATOR, Role::LIMITED_ADMINISTRATOR, Role::BASIC_END_USER);
         }
-        if (!isset($user->superset_id)) { // Automatically create a proper superset account for all users
+        /* if (!isset($user->superset_id)) { // Automatically create a proper superset account for all users
             $json = SupersetApiUtils::get_or_add_user($user);
             $user->superset_id = $json['id'] ?? null;
             $user->save();
-        }
+        } */
         return $user;
     }
 
