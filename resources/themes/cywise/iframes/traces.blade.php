c0afa3821797bb96ed792702c2c01c5274752736
csavelief <csavelief@mncc.fr>
2025-09-11 18:00:23 +0200

    Group and display traces by interval of 5 minutes

--- resources/themes/cywise/iframes/traces.blade.php
+++ resources/themes/cywise/iframes/traces.blade.php
@@ -1,6 +1,58 @@
 @extends('theme::iframes.app')
 
 @section('content')
+@if($traces->isNotEmpty())
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/charts.css/dist/charts.min.css">
+<div class="card mt-3 mb-3">
+  <div class="card-body">
+    <div class="card-text">
+      <table
+        class="charts-css column hide-data show-primary-axis show-4-secondary-axes data-spacing-1 multiple stacked">
+        <thead>
+        <tr>
+          <th scope="col">{{ __('Timestamp') }}</th>
+          <th scope="col">{{ __('Successes') }}</th>
+          <th scope="col">{{ __('Failures') }}</th>
+        </tr>
+        </thead>
+        @php
+        $groups = $traces->sortBy('created_at')->groupBy(fn ($trace) =>
+        "[{$trace->created_at->floorMinute(5)->format('Y-m-d H:i')}, {$trace->created_at->ceilMinute(5)->format('Y-m-d
+        H:i')}]")
+        ->map(function ($group) {
+        return [
+        'success' => $group->where('failed', false)->count(),
+        'failure' => $group->where('failed', true)->count(),
+        ];
+        });
+        $maxCount = $groups->map(fn($group) => $group['success'] + $group['failure'])->max();
+        @endphp
+        <tbody style="height: 200px">
+        @foreach($groups as $timestamp => $counts)
+        <tr>
+          <th scope="row">{{ $timestamp }}</th>
+          <td
+            style="--size: {{ $counts['success'] / $maxCount }}; --color: var(--ds-background-success); border: {{ $counts['success'] === 0 ? 'none' : '1px solid var(--ds-text-success)' }}; border-bottom: none">
+            <span class="data">{{ $counts['success'] }}</span>
+            <span class="tooltip text-left">
+              {{ __('Successes') }}: {{ $counts['success'] }}<br>{{ $timestamp }}
+            </span>
+          </td>
+          <td
+            style="--size: {{ $counts['failure'] / $maxCount }}; --color: var(--ds-background-danger); border: {{ $counts['failure'] === 0 ? 'none' : '1px solid var(--ds-text-danger)' }}; border-bottom: none">
+            <span class="data">{{ $counts['failure'] }}</span>
+            <span class="tooltip">
+              {{ __('Failures') }}: {{ $counts['failure'] }}<br>{{ $timestamp }}
+            </span>
+          </td>
+        </tr>
+        @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+@endif
 <div class="card mt-3 mb-3">
   @if($traces->isEmpty())
   <div class="card-body">
@@ -11,7 +63,7 @@
     </div>
   </div>
   @else
-  <div class="card-body p-0">
+  <div class="card-body p-0 mt-3">
     <table class="table table-hover mb-0">
       <thead>
       <tr>
