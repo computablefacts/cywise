35417796af9586a8ccfbf03b64160435239fff7e
Patrick Brisacier <pbrisacier@mncc.fr>
2025-09-26 14:23:53 +0200

    DB app config: add default values (used by testing env for PHP Unit)

--- database/seeders/DbConfig/DefaultDbAppConfig.php
+++ database/seeders/DbConfig/DefaultDbAppConfig.php
@@ -6,6 +6,40 @@ class DefaultDbAppConfig implements DbAppConfigInterface
 {
     public function getParams(): array
     {
-        return [];
+        return [
+            'towerify.cyberbuddy.api' => 'https://dev.generic-rag.dev02.towerify.io',
+            'towerify.cyberbuddy.api_username' => 'wrong',
+            'towerify.cyberbuddy.api_password' => 'wrong',
+
+            'towerify.stripe.plans.essential.name' => 'Essentiel',
+            'towerify.stripe.plans.essential.description' => null,
+            'towerify.stripe.plans.essential.features' => '',
+            'towerify.stripe.plans.essential.monthly_price' => null,
+            'towerify.stripe.plans.essential.monthly_price_id' => null,
+            'towerify.stripe.plans.essential.yearly_price' => null,
+            'towerify.stripe.plans.essential.yearly_price_id' => null,
+            'towerify.stripe.plans.essential.onetime_price' => null,
+            'towerify.stripe.plans.essential.onetime_price_id' => null,
+
+            'towerify.stripe.plans.standard.name' => 'Standard',
+            'towerify.stripe.plans.standard.description' => null,
+            'towerify.stripe.plans.standard.features' => '',
+            'towerify.stripe.plans.standard.monthly_price' => null,
+            'towerify.stripe.plans.standard.monthly_price_id' => null,
+            'towerify.stripe.plans.standard.yearly_price' => null,
+            'towerify.stripe.plans.standard.yearly_price_id' => null,
+            'towerify.stripe.plans.standard.onetime_price' => null,
+            'towerify.stripe.plans.standard.onetime_price_id' => null,
+
+            'towerify.stripe.plans.premium.name' => 'Premium',
+            'towerify.stripe.plans.premium.description' => null,
+            'towerify.stripe.plans.premium.features' => '',
+            'towerify.stripe.plans.premium.monthly_price' => null,
+            'towerify.stripe.plans.premium.monthly_price_id' => null,
+            'towerify.stripe.plans.premium.yearly_price' => null,
+            'towerify.stripe.plans.premium.yearly_price_id' => null,
+            'towerify.stripe.plans.premium.onetime_price' => null,
+            'towerify.stripe.plans.premium.onetime_price_id' => null,
+        ];
     }
 }
