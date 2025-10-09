297b2604a0e820f80c93eb996a821d5e80013543
Patrick Brisacier <pbrisacier@mncc.fr>
2025-10-09 14:51:48 +0200

    Display translations for Monthly/Yearly in billing/billing_cycle_toggle view

--- resources/views/components/billing/billing_cycle_toggle.blade.php
+++ resources/views/components/billing/billing_cycle_toggle.blade.php
@@ -9,15 +9,15 @@
         }, 1);
     "
      @reposition-interval-marker.window="toggleRepositionMarker($refs.monthly);"
-     class="relative w-40 mb-5"
+     class="relative w-56 mb-5"
      x-cloak>
     <div x-ref="toggleButtons" class="relative inline-grid items-center justify-center w-full h-10 grid-cols-2 p-1 bg-white rounded-full shadow-sm select-none dark:bg-neutral-800 ring-1 ring-gray-200 dark:ring-neutral-700">
         <button x-ref="monthly" @click="toggleButtonClicked($el, 'month');" type="button"
                 :class="{ 'text-white' : billing_cycle_selected == 'month', 'text-gray-500 dark:text-neutral-400' : billing_cycle_selected != 'month' }"
-                class="relative z-20 inline-flex items-center justify-center w-full h-8 px-3 text-xs font-semibold transition-all cursor-pointer whitespace-nowrap">Monthly</button>
+                class="relative z-20 inline-flex items-center justify-center w-full h-8 px-3 text-xs font-semibold transition-all cursor-pointer whitespace-nowrap">{{ __('Monthly') }}</button>
         <button x-ref="yearly" @click="toggleButtonClicked($el, 'year');" type="button"
                 :class="{ 'text-white' : billing_cycle_selected == 'year', 'text-gray-500 dark:text-neutral-400' : billing_cycle_selected != 'year' }"
-                class="relative z-20 inline-flex items-center justify-center w-full h-8 px-3 text-xs font-semibold transition-all rounded-md cursor-pointer whitespace-nowrap">Yearly</button>
+                class="relative z-20 inline-flex items-center justify-center w-full h-8 px-3 text-xs font-semibold transition-all rounded-md cursor-pointer whitespace-nowrap">{{ __('Yearly') }}</button>
         <div x-ref="marker" class="absolute left-0 z-10 w-1/2 h-full opacity-0" x-cloak>
             <div @class([
                 'w-full h-full rounded-full shadow-sm',
