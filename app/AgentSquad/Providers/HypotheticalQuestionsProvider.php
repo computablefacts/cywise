2c640ed8e764bfab2051c61562e1fec2d7a02d4b
csavelief <csavelief@mncc.fr>
2025-11-03 14:11:51 +0100

    Cache hypothetical questions

--- app/AgentSquad/Providers/HypotheticalQuestionsProvider.php
+++ app/AgentSquad/Providers/HypotheticalQuestionsProvider.php
@@ -8,26 +8,28 @@ class HypotheticalQuestionsProvider
 {
     public static function provide(string $language, string $text, string $prompt = 'default_hypothetical_questions'): array
     {
-        try {
-            $start = microtime(true);
-            $prompt = PromptsProvider::provide($prompt, [
-                'LANGUAGE' => $language,
-                'TEXT' => $text,
-            ]);
-            $questions = LlmsProvider::provide($prompt, null, 3 * 60);
-            $questions = array_values(array_filter(explode("\n", $questions), fn(string $question) => !empty($question)));
-            $questions = array_map(fn(string $question) => [
-                'question' => $question,
-                'language' => $language,
-                'embedding' => EmbeddingsProvider::provide($question)->embedding(),
-            ], $questions);
-            $stop = microtime(true);
-            Log::debug("[HYPOTHETICAL_QUESTIONS_PROVIDER] " . count($questions) . " questions have been generated in " . ((int)ceil($stop - $start)) . " seconds");
-            return $questions;
-        } catch (\Exception $e) {
-            Log::debug("[HYPOTHETICAL_QUESTIONS_PROVIDER] Questions generation failed");
-            Log::error($e->getMessage());
-            return [];
-        }
+        return \Cache::remember('hypothetical_questions_provider_' . md5($language . $text . $prompt), 7 * 24 * 60, function () use ($language, $text, $prompt) {
+            try {
+                $start = microtime(true);
+                $prompt = PromptsProvider::provide($prompt, [
+                    'LANGUAGE' => $language,
+                    'TEXT' => $text,
+                ]);
+                $questions = LlmsProvider::provide($prompt, null, 3 * 60);
+                $questions = array_values(array_filter(explode("\n", $questions), fn(string $question) => !empty($question)));
+                $questions = array_map(fn(string $question) => [
+                    'question' => $question,
+                    'language' => $language,
+                    'embedding' => EmbeddingsProvider::provide($question)->embedding(),
+                ], $questions);
+                $stop = microtime(true);
+                Log::debug("[HYPOTHETICAL_QUESTIONS_PROVIDER] " . count($questions) . " questions have been generated in " . ((int)ceil($stop - $start)) . " seconds");
+                return $questions;
+            } catch (\Exception $e) {
+                Log::debug("[HYPOTHETICAL_QUESTIONS_PROVIDER] Questions generation failed");
+                Log::error($e->getMessage());
+                return [];
+            }
+        });
     }
 }
