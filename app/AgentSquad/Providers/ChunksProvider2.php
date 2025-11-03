746d421e049f0c30c276a6163836a93bb2f8acdf
csavelief <csavelief@mncc.fr>
2025-11-03 10:09:14 +0100

    Cache chunks (vector)

--- app/AgentSquad/Providers/ChunksProvider2.php
+++ app/AgentSquad/Providers/ChunksProvider2.php
@@ -17,53 +17,55 @@ class ChunksProvider2
         if ($collections->isEmpty() || empty($language) || empty($input)) {
             return collect();
         }
-        try {
-            $start = microtime(true);
-            if (Vector::isSupportedByMariaDb()) {
-                $embedding = json_encode(EmbeddingsProvider::provide($input)?->embedding() ?? []);
-                $chunks = collect(DB::select("
-                    SELECT DISTINCT
-                      chunk_id, 
-                      (1 - VEC_DISTANCE_COSINE(VEC_FromText('{$embedding}'), embedding)) AS similarity
-                    FROM cb_vectors
-                    WHERE locale = '{$language}'
-                    AND collection_id IN ({$collections->pluck('id')->implode(',')})
-                    ORDER BY VEC_DISTANCE_COSINE(VEC_FromText('{$embedding}'), embedding)
-                    LIMIT {$take}
-                "))->map(function (object $vector) {
-                    /** @var Chunk $chunk */
-                    $chunk = Chunk::findOrFail($vector->chunk_id);
-                    $chunk->_score = $vector->similarity;
-                    return $chunk;
-                });
-            } else {
-                $embedding = EmbeddingsProvider::provide($input)?->embedding() ?? [];
-                $vectorStore = new MemoryVectorStore($take);
-                $vectorStore->addVectors(collect(DB::select("
-                    SELECT DISTINCT *
-                    FROM cb_vectors
-                    WHERE locale = '{$language}'
-                    AND collection_id IN ({$collections->pluck('id')->implode(',')})
-                "))->map(fn(object $vector) => new \App\AgentSquad\Vectors\Vector(
-                    $vector->hypothetical_question,
-                    json_decode($vector->embedding, true),
-                    ['chunk_id' => $vector->chunk_id]
-                ))->toArray());
-                $chunks = collect($vectorStore->search($embedding))
-                    ->map(function (array $vector) {
+        return \Cache::remember('chunks_provider2_' . md5($collections->pluck('id')->implode('_') . "{$language}{$input}"), 7 * 24 * 60, function () use ($collections, $language, $input, $take) {
+            try {
+                $start = microtime(true);
+                if (Vector::isSupportedByMariaDb()) {
+                    $embedding = json_encode(EmbeddingsProvider::provide($input)?->embedding() ?? []);
+                    $chunks = collect(DB::select("
+                        SELECT DISTINCT
+                          chunk_id, 
+                          (1 - VEC_DISTANCE_COSINE(VEC_FromText('{$embedding}'), embedding)) AS similarity
+                        FROM cb_vectors
+                        WHERE locale = '{$language}'
+                        AND collection_id IN ({$collections->pluck('id')->implode(',')})
+                        ORDER BY VEC_DISTANCE_COSINE(VEC_FromText('{$embedding}'), embedding)
+                        LIMIT {$take}
+                    "))->map(function (object $vector) {
                         /** @var Chunk $chunk */
-                        $chunk = Chunk::findOrFail($vector['vector']->metadata('chunk_id'));
-                        $chunk->_score = $vector['similarity'];
+                        $chunk = Chunk::findOrFail($vector->chunk_id);
+                        $chunk->_score = $vector->similarity;
                         return $chunk;
                     });
+                } else {
+                    $embedding = EmbeddingsProvider::provide($input)?->embedding() ?? [];
+                    $vectorStore = new MemoryVectorStore($take);
+                    $vectorStore->addVectors(collect(DB::select("
+                        SELECT DISTINCT *
+                        FROM cb_vectors
+                        WHERE locale = '{$language}'
+                        AND collection_id IN ({$collections->pluck('id')->implode(',')})
+                    "))->map(fn(object $vector) => new \App\AgentSquad\Vectors\Vector(
+                        $vector->hypothetical_question,
+                        json_decode($vector->embedding, true),
+                        ['chunk_id' => $vector->chunk_id]
+                    ))->toArray());
+                    $chunks = collect($vectorStore->search($embedding))
+                        ->map(function (array $vector) {
+                            /** @var Chunk $chunk */
+                            $chunk = Chunk::findOrFail($vector['vector']->metadata('chunk_id'));
+                            $chunk->_score = $vector['similarity'];
+                            return $chunk;
+                        });
+                }
+                $stop = microtime(true);
+                Log::debug("[CHUNKS_PROVIDER_2] Search for '$input' took " . ((int)ceil($stop - $start)) . " seconds and returned {$chunks->count()} results");
+                return $chunks;
+            } catch (\Exception $e) {
+                Log::debug("[CHUNKS_PROVIDER_2] Search for '$input' failed");
+                Log::error($e->getMessage());
+                return collect();
             }
-            $stop = microtime(true);
-            Log::debug("[CHUNKS_PROVIDER_2] Search for '$input' took " . ((int)ceil($stop - $start)) . " seconds and returned {$chunks->count()} results");
-            return $chunks;
-        } catch (\Exception $e) {
-            Log::debug("[CHUNKS_PROVIDER_2] Search for '$input' failed");
-            Log::error($e->getMessage());
-            return collect();
-        }
+        });
     }
 }
