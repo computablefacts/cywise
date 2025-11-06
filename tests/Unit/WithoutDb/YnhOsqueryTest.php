afc52c26296cf95aab956e0c961546f898d3c648
Patrick Brisacier <pbrisacier@mncc.fr>
2025-11-06 12:30:13 +0100

    Tests: refactor one test with Higher Order Testing style

--- tests/Unit/WithoutDb/YnhOsqueryTest.php
+++ tests/Unit/WithoutDb/YnhOsqueryTest.php
@@ -2,24 +2,18 @@
 
 use App\Models\YnhOsquery;
 
-test('compute columns uid on array', function () {
-    $uid = YnhOsquery::computeColumnsUid(["a", "b", "c"]);
-    expect($uid)->toEqual("b05555a5d6d4b64fad478a407d21ffd1");
+test('compute columns uid on array')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['a', 'b', 'c']))
+    ->toEqual('b05555a5d6d4b64fad478a407d21ffd1')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['b', 'a', 'c']))
+    ->toEqual('b919231a1aec8102cacdbf55bf397471');
 
-    $uid = YnhOsquery::computeColumnsUid(["b", "a", "c"]);
-    expect($uid)->toEqual("b919231a1aec8102cacdbf55bf397471");
-});
-
-test('compute columns uid on associative array', function () {
-    $uid = YnhOsquery::computeColumnsUid(["id" => 1, "array" => ["id" => 1, "array" => ["a", "b", "c"]]]);
-    expect($uid)->toEqual("bf1823f8153cfc357a0bfa61e1e2c6a4");
-
-    $uid = YnhOsquery::computeColumnsUid(["array" => ["id" => 1, "array" => ["a", "b", "c"]], "id" => 1]);
-    expect($uid)->toEqual("bf1823f8153cfc357a0bfa61e1e2c6a4");
-
-    $uid = YnhOsquery::computeColumnsUid(["id" => 1, "array" => ["id" => 1, "array" => ["b", "a", "c"]]]);
-    expect($uid)->toEqual("fb7224001217899d790af7b30f4d8b55");
-
-    $uid = YnhOsquery::computeColumnsUid(["id" => 1, "array" => ["array" => ["b", "a", "c"], "id" => 1]]);
-    expect($uid)->toEqual("fb7224001217899d790af7b30f4d8b55");
-});
+test('compute columns uid on associative array')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['id' => 1, 'array' => ['id' => 1, 'array' => ['a', 'b', 'c']]]))
+    ->toEqual('bf1823f8153cfc357a0bfa61e1e2c6a4')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['array' => ['id' => 1, 'array' => ['a', 'b', 'c']], 'id' => 1]))
+    ->toEqual('bf1823f8153cfc357a0bfa61e1e2c6a4')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['id' => 1, 'array' => ['id' => 1, 'array' => ['b', 'a', 'c']]]))
+    ->toEqual('fb7224001217899d790af7b30f4d8b55')
+    ->expect(fn () => YnhOsquery::computeColumnsUid(['id' => 1, 'array' => ['array' => ['b', 'a', 'c'], 'id' => 1]]))
+    ->toEqual('fb7224001217899d790af7b30f4d8b55');
