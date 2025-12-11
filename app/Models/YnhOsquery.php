8f279cfd6b37e61d13a1a49cbd7bf02025937108
csavelief <csavelief@mncc.fr>
2025-12-11 17:54:46 +0100

    Mark functions as deprecated

--- app/Models/YnhOsquery.php
+++ app/Models/YnhOsquery.php
@@ -81,6 +81,7 @@ class YnhOsquery extends Model
         return md5($uid);
     }
 
+    /** @deprecated */
     public static function configLogParserLinux(YnhServer $server): string
     {
         $url = app_url();
@@ -204,6 +205,7 @@ fi
 EOT;
     }
 
+    /** @deprecated */
     public static function configLogParserWindows(YnhServer $server): string
     {
         $url = app_url();
