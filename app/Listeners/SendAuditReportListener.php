06e8f1371e0e60d5dca6c8cc8efe6afe0b9de678
Patrick Brisacier <pbrisacier@mncc.fr>
2025-12-18 20:38:56 +0100

    AuditReport: better plural

--- app/Listeners/SendAuditReportListener.php
+++ app/Listeners/SendAuditReportListener.php
@@ -23,7 +23,7 @@ class SendAuditReportListener extends AbstractListener
 
     protected function handle2($event)
     {
-        if (!($event instanceof SendAuditReport)) {
+        if (! ($event instanceof SendAuditReport)) {
             throw new \Exception('Invalid event type!');
         }
 
@@ -33,7 +33,7 @@ class SendAuditReportListener extends AbstractListener
         $from = 'cyberbuddy@cywise.io'; // config('towerify.freshdesk.from_email');
         $to = $user->email;
 
-        if (!$user->gets_audit_report) {
+        if (! $user->gets_audit_report) {
             return;
         }
 
@@ -47,9 +47,9 @@ class SendAuditReportListener extends AbstractListener
         $leaks = $this->buildSectionLeaks($user);
         $vulns = $this->buildSectionVulns($assets);
         $subject = $this->buildEmailSubject($user, $assets);
-        $body = ["<table cellspacing=\"0\" cellpadding=\"0\" style=\"margin: auto;\"><tbody>"];
-        $body[] = "<tr><td style=\"font-size: 28px; text-align: center;\">Bonjour !</td></tr>";
-        $body[] = "<tr><td style=\"font-size: 16px; line-height: 1.6;\">";
+        $body = ['<table cellspacing="0" cellpadding="0" style="margin: auto;"><tbody>'];
+        $body[] = '<tr><td style="font-size: 28px; text-align: center;">Bonjour !</td></tr>';
+        $body[] = '<tr><td style="font-size: 16px; line-height: 1.6;">';
         $body[] = $summary;
         $body[] = empty($leaks) && empty($vulns) ?
             "<p>Félicitations ! Vous n'avez aucune action à entreprendre.</p>" :
@@ -58,17 +58,17 @@ class SendAuditReportListener extends AbstractListener
         $body[] = $leaks;
 
         if ($isOnboarding) {
-            $body[] = "<p>Pour découvrir comment corriger vos vulnérabilités et renforcer la sécurité de votre infrastructure, finalisez votre inscription à Cywise :</p>";
-            $body[] = "</td></tr>";
+            $body[] = '<p>Pour découvrir comment corriger vos vulnérabilités et renforcer la sécurité de votre infrastructure, finalisez votre inscription à Cywise :</p>';
+            $body[] = '</td></tr>';
             $body[] = $this->buildEmailCta($user);
-            $body[] = "<tr><td style=\"font-size: 16px; line-height: 1.6;\">";
+            $body[] = '<tr><td style="font-size: 16px; line-height: 1.6;">';
         }
 
-        $body[] = "<p>Je reste à votre disposition pour toute question ou assistance supplémentaire. Merci encore pour votre confiance en Cywise !</p>";
-        $body[] = "<p>Bien à vous,</p>";
-        $body[] = "<p>CyberBuddy</p>";
-        $body[] = "</td></tr>";
-        $body[] = "</tbody></table>";
+        $body[] = '<p>Je reste à votre disposition pour toute question ou assistance supplémentaire. Merci encore pour votre confiance en Cywise !</p>';
+        $body[] = '<p>Bien à vous,</p>';
+        $body[] = '<p>CyberBuddy</p>';
+        $body[] = '</td></tr>';
+        $body[] = '</tbody></table>';
 
         MailCoachSimpleEmail::sendEmail($subject, '', implode("\n", $body), $to, $from);
     }
@@ -81,6 +81,7 @@ class SendAuditReportListener extends AbstractListener
             'reason' => 'Finalisez votre inscription en créant un mot de passe',
             'action' => 'Créer mon mot de passe',
         ]);
+
         return "
             <tr>
                 <td align=\"center\" style=\"background-color: #fbca3e; padding: 10px 20px; border-radius: 5px;\">                    
@@ -97,26 +98,26 @@ class SendAuditReportListener extends AbstractListener
         $nbNewAssets = Asset::where('created_at', '>=', Carbon::now()->subDay())->count();
 
         $nbLeaks = TimelineController::fetchLeaks($user)
-            ->flatMap(fn(TimelineItem $item) => json_decode($item->attributes()['credentials']))
+            ->flatMap(fn (TimelineItem $item) => json_decode($item->attributes()['credentials']))
             ->unique()
             ->count();
 
-        $nbHigh = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbHigh = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
-        $nbMedium = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbMedium = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
-        $nbLow = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityLow()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbLow = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityLow()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
         $nbIssues = $nbNewAssets + $nbLeaks + $nbHigh + $nbMedium + $nbLow;
 
         if ($nbIssues === 0) {
-            return "Cywise - Tout va bien !";
+            return 'Cywise - Tout va bien !';
         }
         if ($nbHigh > 0) {
             return "Cywise - {$nbHigh} vulnérabilités doivent être corrigées !";
@@ -130,7 +131,8 @@ class SendAuditReportListener extends AbstractListener
         if ($nbNewAssets > 0) {
             return "Cywise - {$nbNewAssets} nouveaux actifs ont été ajoutés !";
         }
-        return "Cywise - Une fuite de données ou compromission a été détectée !";
+
+        return 'Cywise - Une fuite de données ou compromission a été détectée !';
     }
 
     private function buildSummary(User $user, Collection $assets): string
@@ -139,58 +141,69 @@ class SendAuditReportListener extends AbstractListener
             ->count();
 
         $nbLeaks = TimelineController::fetchLeaks($user)
-            ->flatMap(fn(TimelineItem $item) => json_decode($item->attributes()['credentials']))
+            ->flatMap(fn (TimelineItem $item) => json_decode($item->attributes()['credentials']))
             ->unique()
             ->count();
 
-        $nbDns = $assets->filter(fn(Asset $asset) => $asset->is_monitored && $asset->isDns())
+        $nbDns = $assets->filter(fn (Asset $asset) => $asset->is_monitored && $asset->isDns())
             ->pluck('asset')
             ->unique()
             ->count();
 
-        $nbIpAddresses = $assets->filter(fn(Asset $asset) => $asset->is_monitored)
-            ->flatMap(fn(Asset $asset) => $asset->ports()->get())
+        $nbIpAddresses = $assets->filter(fn (Asset $asset) => $asset->is_monitored)
+            ->flatMap(fn (Asset $asset) => $asset->ports()->get())
             ->pluck('ip')
             ->unique()
             ->count();
 
-        $nbHigh = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbHigh = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
-        $nbMedium = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbMedium = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
-        $nbLow = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityLow()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0)
+        $nbLow = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityLow()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0)
             ->count();
 
         $nbAlerts = $nbHigh + $nbMedium + $nbLow;
 
-        $newAssets = $nbNewAssets === 0 ?
-            '' :
-            "<li>J'ai mis sous surveillance <b>{$nbNewAssets}</b> nouveaux actifs durant ces dernières 24h.</li>";
+        $newAssets = match ($nbNewAssets) {
+            0 => '',
+            1 => "<li>J'ai mis sous surveillance <b>{$nbNewAssets}</b> nouvel actif durant ces dernières 24h.</li>",
+            default => "<li>J'ai mis sous surveillance <b>{$nbNewAssets}</b> nouveaux actifs durant ces dernières 24h.</li>",
+        };
 
-        $leaks = $nbLeaks === 0 ?
-            '' :
-            "<li>J'ai trouvé <b>{$nbLeaks}</b> identifiants fuités ou compromis.</li>";
+        $leaks = match ($nbLeaks) {
+            0 => '',
+            1 => "<li>J'ai trouvé <b>{$nbLeaks}</b> identifiant fuité ou compromis.</li>",
+            default => "<li>J'ai trouvé <b>{$nbLeaks}</b> identifiants fuités ou compromis.</li>",
+        };
 
-        $perimeter = $nbDns + $nbIpAddresses === 0 ?
-            '' :
-            "<li>J'ai analysé <b>{$nbDns}</b> domaines et <b>{$nbIpAddresses}</b> serveurs.</li>";
+        $perimeter = match ($nbDns + $nbIpAddresses) {
+            0 => '',
+            default => "<li>J'ai analysé <b>{$nbDns}</b> domaine" . ($nbDns > 1 ? 's' : '') . " et <b>{$nbIpAddresses}</b> serveur" . ($nbIpAddresses > 1 ? 's' : '') . ".</li>",
+        };
 
-        $high = $nbHigh === 0 ?
-            '' :
-            "<li><b>{$nbHigh}</b> vulnérabilités critiques <b>doivent</b> être corrigées.</li>";
+        $high = match ($nbHigh) {
+            0 => '',
+            1 => "<li><b>{$nbHigh}</b> vulnérabilité critique <b>doit</b> être corrigée.</li>",
+            default => "<li><b>{$nbHigh}</b> vulnérabilités critiques <b>doivent</b> être corrigées.</li>",
+        };
 
-        $medium = $nbMedium === 0 ?
-            '' :
-            "<li><b>{$nbMedium}</b> vulnérabilités de criticité moyenne <b>devraient</b> être corrigées.</li>";
+        $medium = match ($nbMedium) {
+            0 => '',
+            1 => "<li><b>{$nbMedium}</b> vulnérabilité de criticité moyenne <b>devrait</b> être corrigée.</li>",
+            default => "<li><b>{$nbMedium}</b> vulnérabilités de criticité moyenne <b>devraient</b> être corrigées.</li>",
+        };
 
-        $low = $nbLow === 0 ?
-            '' :
-            "<li><b>{$nbLow}</b> vulnérabilités de criticité basse ne posent pas un risque de sécurité immédiat.</li>";
+        $low = match ($nbLow) {
+            0 => '',
+            1 => "<li><b>{$nbLow}</b> vulnérabilité de criticité basse ne pose pas un risque de sécurité immédiat.</li>",
+            default => "<li><b>{$nbLow}</b> vulnérabilités de criticité basse ne posent pas un risque de sécurité immédiat.</li>",
+        };
 
         $vulns = $nbAlerts === 0 ?
             '' :
@@ -213,18 +226,20 @@ class SendAuditReportListener extends AbstractListener
     private function buildSectionLeaks(User $user): string
     {
         $leaks = TimelineController::fetchLeaks($user)
-            ->flatMap(fn(TimelineItem $item) => json_decode($item->attributes()['credentials']))
+            ->flatMap(fn (TimelineItem $item) => json_decode($item->attributes()['credentials']))
             ->sortBy('leak_date', SORT_NATURAL | SORT_FLAG_CASE)
             ->reverse()
             ->map(function (object $leak) {
                 $date = empty($leak->leak_date) ? '' : " (date est. {$leak->leak_date})";
                 $password = empty($leak->password) ? '' : " ({$leak->password})";
+
                 return empty($leak->website) ?
                     "<li>L'identifiant <b>{$leak->email}</b>{$password} a été fuité{$date}.</li>" :
                     "<li>L'identifiant <b>{$leak->email}</b>{$password} donnant accès au site web <b>{$leak->website}</b> a été compromis{$date}.</li>";
             })
             ->unique()
             ->join("\n");
+
         return empty($leaks) ? '' : "
             <h3>Identifiants fuités ou compromis</h3>
             <p>Cywise surveille également les fuites de données et compromissions !<p>
@@ -235,14 +250,14 @@ class SendAuditReportListener extends AbstractListener
 
     private function buildSectionVulns(Collection $assets): string
     {
-        $high = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0);
+        $high = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityHigh()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0);
 
-        $medium = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0);
+        $medium = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityMedium()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0);
 
-        $low = $assets->flatMap(fn(Asset $asset) => $asset->alertsWithCriticalityLow()->get())
-            ->filter(fn(Alert $alert) => $alert->is_hidden === 0);
+        $low = $assets->flatMap(fn (Asset $asset) => $asset->alertsWithCriticalityLow()->get())
+            ->filter(fn (Alert $alert) => $alert->is_hidden === 0);
 
         return $high
             ->concat($medium)
@@ -250,16 +265,16 @@ class SendAuditReportListener extends AbstractListener
             ->map(function (Alert $alert) {
 
                 if ($alert->isHigh()) {
-                    $level = "(criticité haute)";
+                    $level = '(criticité haute)';
                 } elseif ($alert->isMedium()) {
-                    $level = "(criticité moyenne)";
+                    $level = '(criticité moyenne)';
                 } elseif ($alert->isLow()) {
-                    $level = "(criticité basse)";
+                    $level = '(criticité basse)';
                 } else {
-                    $level = "";
+                    $level = '';
                 }
                 if (empty($alert->cve_id)) {
-                    $cve = "";
+                    $cve = '';
                 } else {
                     $cve = "<p><b>Note.</b> Cette vulnérabilité a pour identifiant <a href=\"https://nvd.nist.gov/vuln/detail/{$alert->cve_id}\">{$alert->cve_id}</a>.</p>";
                 }
@@ -280,7 +295,7 @@ class SendAuditReportListener extends AbstractListener
                     $remediation = $result['response'];
                 }
 
-                $link = route('iframes.assets') . "#aid-{$alert->asset()->id}";
+                $link = route('iframes.assets')."#aid-{$alert->asset()->id}";
 
                 return "
                     <h3>{$alert->title} {$level}</h3>
