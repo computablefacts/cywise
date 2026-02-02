2092fdda01b84efab6abff40f8b2564cf2f45634
csavelief <csavelief@gmail.com>
2026-02-02 18:13:10 +0100

    Remove deprecated class App\Helpers\EmbeddingProvider

--- app/AgentSquad/Providers/EmbeddingsProvider.php
+++ app/AgentSquad/Providers/EmbeddingsProvider.php
@@ -3,7 +3,7 @@
 namespace App\AgentSquad\Providers;
 
 use App\AgentSquad\Vectors\Vector;
-use App\Helpers\EmbeddingProvider;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class EmbeddingsProvider
@@ -13,8 +13,7 @@ class EmbeddingsProvider
         return \Cache::remember('embeddings_provider_' . md5($text), 7 * 24 * 60, function () use ($text, $metadata) {
             try {
                 $start = microtime(true);
-                $provider = new EmbeddingProvider(EmbeddingProvider::DEEP_INFRA);
-                $embedding = $provider->execute($text)['data'][0]['embedding'] ?? [];
+                $embedding = self::callDeepInfra($text)['data'][0]['embedding'] ?? [];
                 $vector = new Vector($text, $embedding, $metadata);
                 $stop = microtime(true);
                 Log::debug("[EMBEDDINGS_PROVIDER] Computing embeddings took " . ((int)ceil($stop - $start)) . " seconds");
@@ -25,5 +24,39 @@ class EmbeddingsProvider
                 return null;
             }
         });
+    }
+
+    private static function callDeepInfra(string $text, ?string $model = null): array
+    {
+        return self::post(config('towerify.deepinfra.api') . '/embeddings', config('towerify.deepinfra.api_key'), $text, $model ?? 'BAAI/bge-m3-multi');
+    }
+
+    private static function post(string $url, string $bearer, string $text, string $model): array
+    {
+        try {
+
+            $payload = [
+                'model' => $model,
+                'input' => $text,
+                'encoding_format' => 'float',
+            ];
+
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer {$bearer}",
+                'Accept' => 'application/json',
+            ])
+                ->timeout(60)
+                ->post($url, $payload);
+
+            if ($response->successful()) {
+                $json = $response->json();
+                // Log::debug($json);
+                return $json;
+            }
+            Log::error($response->body());
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        return [];
     }
 }
