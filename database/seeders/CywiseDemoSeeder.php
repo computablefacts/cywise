5f34bfc49c3818147af66b3bf087d82181edaba1
Patrick Brisacier <pbrisacier@mncc.fr>
2026-02-03 12:24:44 +0100

    Add Performa setting when creating demo user

--- database/seeders/CywiseDemoSeeder.php
+++ database/seeders/CywiseDemoSeeder.php
@@ -10,11 +10,24 @@ class CywiseDemoSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-     * @return void
+     * @return void 
      */
     public function run(): void
     {
-        User::getOrCreate('demo@mydomain.com', 'Cywise Démo', 'DemoPass2026');
+        $demoUser = User::getOrCreate('demo@mydomain.com', 'Cywise Démo', 'DemoPass2026');
+        $demoUser->performa_domain = $this->performaDomainFromEnvVar();
+        $demoUser->performa_secret = env('PERFORMA_SECRET_KEY', 'performa_secret');
+        $demoUser->save();
     }
 
+    private function performaDomainFromEnvVar() 
+    {
+        $url = env('PERFORMA_BASE_APP_URL', 'http://localhost:17802');
+
+        $parsedUrl = parse_url($url);
+        $host = $parsedUrl['host'] ?? null;
+        $port = $parsedUrl['port'] ?? null;
+
+        return $host . ($port ? ":$port" : '');
+    }
 }
