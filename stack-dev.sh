e58e129ffe47e6789c46890c62fab2a46c24df10
Patrick Brisacier <pbrisacier@mncc.fr>
2026-02-18 10:53:27 +0100

    Better script to start stack for dev (allow to pass parameters to docker compose like 'logs --follow')

--- stack-dev.sh
+++ stack-dev.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Fichiers Docker Compose pour le developpement
+# Fichiers Docker Compose pour le développement
 DEV_DOCKER_COMPOSE_FILES="-f docker-compose.yaml -f docker-compose.dev.yaml"
 
-# Définir les arguments personnalisés pour docker compose up
-export DOCKER_COMPOSE_ARGS="${DEV_DOCKER_COMPOSE_FILES} up --build --detach"
+# Si aucun argument n'est passé
+if [ $# -eq 0 ]; then
+    # Définir les arguments personnalisés pour docker compose up
+    export DOCKER_COMPOSE_ARGS="${DEV_DOCKER_COMPOSE_FILES} up --build --detach"
+    source ./stack-start.sh
 
-source ./stack-start.sh
+    echo -e "\nLe mode 'watch' va démarrer."
+    echo -e "\nVous pouvez voir les logs en lançant dans un autre terminal :"
+    echo -e "  docker compose logs --timestamps --follow"
 
-echo -e "\nLe mode 'watch' va démarrer."
-echo -e "\nVous pouvez voir les logs en lançant dans un autre terminal :"
-echo -e "  docker compose logs --timestamps --follow"
-
-docker compose $DEV_DOCKER_COMPOSE_FILES watch --no-up
+    docker compose $DEV_DOCKER_COMPOSE_FILES watch --no-up
+else
+    # Exécuter docker compose avec les arguments passés
+    docker compose $DEV_DOCKER_COMPOSE_FILES "$@"
+fi
