74209e4345f387edce54d56aab4d3ced13d1b003
csavelief <csavelief@gmail.com>
2026-03-17 19:36:28 +0100

    Remove `EmbeddingsProvider`, `HypotheticalQuestionsProvider`, and `TranslationsProvider` implementations and fully replace their functionalities with `ChunkAssistant`.

--- app/AgentSquad/ChunkAssistant.php
+++ app/AgentSquad/ChunkAssistant.php
@@ -2,20 +2,26 @@
 
 namespace App\AgentSquad;
 
-use App\AgentSquad\Providers\EmbeddingsProvider;
-use App\AgentSquad\Providers\HypotheticalQuestionsProvider;
-use App\AgentSquad\Providers\TranslationsProvider;
 use App\AgentSquad\Vectors\Vector;
 use App\Enums\LanguageEnum;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ChunkAssistant
 {
     private string $chunk;
+    private int $timeoutInSeconds = 60;
     private LanguageEnum $lang = LanguageEnum::FRENCH;
 
     public static function use(): ChunkAssistant
     {
         return new ChunkAssistant();
+    }
+
+    public function withTimeout(int $timeoutInSeconds): ChunkAssistant
+    {
+        $this->timeoutInSeconds = $timeoutInSeconds <= 0 ? 60 : $timeoutInSeconds;
+        return $this;
     }
 
     public function withChunk(string $chunk): ChunkAssistant
@@ -34,12 +40,59 @@ class ChunkAssistant
     public function translate(LanguageEnum $lang = LanguageEnum::FRENCH): string
     {
         // Here, the assumption is that $this->chunk is in english, e.g. $this->lang = LanguageEnum::ENGLISH, and $lang is the target language
-        return TranslationsProvider::provide($this->chunk, $lang);
+        if (empty($this->chunk)) {
+            return '';
+        }
+        if ($lang === $this->lang) {
+            return $this->chunk;
+        }
+
+        $key = 'translation:' . $this->lang->value . ':' . $lang->value . ':' . md5($this->chunk);
+
+        return \Cache::remember($key, now()->addDays(120), function () use ($lang) {
+
+            $answer = TextAssistant::use()
+                ->withTimeout($this->timeoutInSeconds)
+                ->withPrompt('default_translate', [
+                    'TEXT' => $this->chunk,
+                    'LANG' => $lang->value,
+                ])
+                ->withDeepInfra('meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo')
+                ->text();
+
+            if ($answer === '') {
+                Log::warning("Unable to translate {$this->chunk} from {$this->lang->value} to {$lang->value} language.");
+                return $this->chunk;
+            }
+            return $answer;
+        });
     }
 
     public function hypotheticalQuestions(): array
     {
-        return HypotheticalQuestionsProvider::provide($this->lang->value, $this->chunk);
+        $key = 'hypothetical_questions_provider_' . md5($this->lang->value . $this->chunk);
+
+        return \Cache::remember($key, now()->addDays(7), function () {
+
+            $questions = TextAssistant::use()
+                ->withTimeout($this->timeoutInSeconds)
+                ->withPrompt('default_hypothetical_questions', [
+                    'LANGUAGE' => $this->lang->value,
+                    'TEXT' => $this->chunk,
+                ])
+                ->text();
+
+            $questions = array_values(array_filter(explode("\n", $questions), fn(string $question) => !empty($question)));
+
+            return array_map(fn(string $question) => [
+                'question' => $question,
+                'language' => $this->lang->value,
+                'embedding' => ChunkAssistant::use()
+                    ->withTimeout($this->timeoutInSeconds)
+                    ->withChunk($question)
+                    ->embedding(),
+            ], $questions);
+        });
     }
 
     public function embedding(array $metadata = []): array
@@ -49,6 +102,40 @@ class ChunkAssistant
 
     public function vector(array $metadata = []): ?Vector
     {
-        return EmbeddingsProvider::provide($this->chunk, $metadata);
+        $key = 'embeddings_provider_' . md5($this->chunk);
+
+        return \Cache::remember($key, now()->addDays(7), function () use ($metadata) {
+            $embedding = $this->callDeepInfra()['data'][0]['embedding'] ?? [];
+            return empty($embedding) ? null : new Vector($this->chunk, $embedding, $metadata);
+        });
+    }
+
+    private function callDeepInfra(): array
+    {
+        try {
+
+            $url = config('towerify.deepinfra.api') . '/embeddings';
+            $bearer = config('towerify.deepinfra.api_key');
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer {$bearer}",
+                'Accept' => 'application/json',
+            ])
+                ->timeout($this->timeoutInSeconds)
+                ->post($url, [
+                    'model' => 'BAAI/bge-m3-multi',
+                    'input' => $this->chunk,
+                    'encoding_format' => 'float',
+                ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            }
+
+            Log::error($response->body());
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        return [];
     }
 }
