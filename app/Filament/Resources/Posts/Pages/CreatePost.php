d5beda66ce3126b5f930de2c59e187a8a36a244a
csavelief <csavelief@gmail.com>
2026-05-05 10:49:36 +0200

    Revert "Wave - Automatically rebuild sitemap."
    This reverts commit 356aae2b4546f58fec42c88eeffdc088b3c1067f.

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -8,9 +8,4 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
-
-    protected function afterCreate(): void
-    {
-        PostResource::rebuildSitemap();
-    }
 }
