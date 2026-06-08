56cc6f592770eea5cfe2a39e51edc9a29eb53df0
csavelief <csavelief@gmail.com>
2026-06-08 21:44:26 +0200

    Documentation - Replace iframe-based implementation with Blade template, update routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use App\Enums\OsqueryPlatformEnum;
 use App\Events\RebuildLatestEventsCache;
 use App\Events\RebuildPackagesList;
 use App\Helpers\SshKeyPair;
-use App\Http\Controllers\Iframes\DocumentationController;
 use App\Http\Controllers\Iframes\DocumentsController;
 use App\Http\Controllers\Iframes\FrameworksController;
 use App\Http\Controllers\Iframes\PromptsController;
@@ -502,7 +501,6 @@ Route::get('/website', [WebsiteController::class, '__invoke'])->name('iframes.we
 Route::middleware([LogHttpRequests::class, 'auth', CheckPermissionsHttpRequest::class])->prefix('iframes')->name('iframes.')->group(function () {
     Route::get('/assets', [TimelineController::class, '__invoke'])->name('assets');
     Route::get('/conversations', [TimelineController::class, '__invoke'])->name('conversations');
-    Route::get('/documentation', [DocumentationController::class, '__invoke'])->name('documentation');
     Route::get('/documents', [DocumentsController::class, '__invoke'])->name('documents');
     Route::get('/events', [TimelineController::class, '__invoke'])->name('events');
     Route::get('/frameworks', [FrameworksController::class, '__invoke'])->name('frameworks');
