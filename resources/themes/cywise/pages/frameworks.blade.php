5b1bbe181010d795069e1fecd79379fb34cb3fc9
csavelief <csavelief@gmail.com>
2026-06-28 20:27:09 +0200

    Refactor frameworks table blade to improve alignment and spacing of action buttons

--- resources/themes/cywise/pages/frameworks.blade.php
+++ resources/themes/cywise/pages/frameworks.blade.php
@@ -107,34 +107,35 @@ render(function (Request $request) {
               <span class="lozenge new">{{ $framework->locale }}</span>
             </td>
             <td class="text-end">
-              @if($framework->loaded())
-              <a href="#" onclick="unload({{ $framework->id }}, event)" class="text-danger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M20 11a8.1 8.1 0 0 0 -11.271 -6.305m-2.41 1.624a8.083 8.083 0 0 0 -1.819 2.681m-.5 -4v4h4"/>
-                  <path d="M4 13a8.1 8.1 0 0 0 13.671 4.691m2.329 -1.691v-1h-1"/>
-                  <path d="M3 3l18 18"/>
-                </svg>
-              </a>
-              @else
-              <a href="#" onclick="load({{ $framework->id }}, event)" class="text-success">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"/>
-                  <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"/>
-                </svg>
-              </a>
-              @endif
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <a data-bs-toggle="collapse" href="#framework{{ $framework->id }}" class="text-decoration-none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M9 6l6 6l-6 6"/>
-                </svg>
-              </a>
+              <div class="d-flex justify-content-end align-items-center">
+                @if($framework->loaded())
+                  <a href="#" onclick="unload({{ $framework->id }}, event)" class="text-danger me-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M20 11a8.1 8.1 0 0 0 -11.271 -6.305m-2.41 1.624a8.083 8.083 0 0 0 -1.819 2.681m-.5 -4v4h4"/>
+                      <path d="M4 13a8.1 8.1 0 0 0 13.671 4.691m2.329 -1.691v-1h-1"/>
+                      <path d="M3 3l18 18"/>
+                    </svg>
+                  </a>
+                @else
+                  <a href="#" onclick="load({{ $framework->id }}, event)" class="text-success me-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"/>
+                      <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"/>
+                    </svg>
+                  </a>
+                @endif
+                <a data-bs-toggle="collapse" href="#framework{{ $framework->id }}" class="text-decoration-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                       stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M9 6l6 6l-6 6"/>
+                  </svg>
+                </a>
+              </div>
             </td>
           </tr>
           <tr class="collapse" id="framework{{ $framework->id }}">
